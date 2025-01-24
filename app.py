6a204154bdbdbab5edea4aca8fc649de43019567
kenguva0812 <kenguvakowshikit@gmail.com>
2025-01-25 02:16:19 +0530

    Initial Flask app with SQL integration

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pyodbc
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return 'Hello World!!, This message is for you...'
+# Database connection
+conn = pyodbc.connect(
+     'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=studentdbserverr.database.windows.net;'
+    'DATABASE=StudentDB;'
+    'UID=adminuser;'
+    'PWD=Kowshik@123'
+)
+cursor = conn.cursor()
+
+@app.route('/')
+def home():
+    return "Hello, Azure Flask App with SQL!"
+
+# Route to fetch data from Students table
+@app.route('/students', methods=['GET'])
+def get_students():
+    query = "SELECT * FROM Students"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    students = [{"ID": row[0], "Name": row[1], "Age": row[2]} for row in rows]
+    return jsonify(students)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
+    app.run(debug=True)
